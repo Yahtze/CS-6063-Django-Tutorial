5f42e3b012d0daa3d915dc0a0a59a689253892f0
Duvuru Yathin Reddy <yathinreddy2004@gmail.com>
2026-02-20 21:25:31 -0500

    Fixed Problems with RDS

--- mysite/settings.py
+++ mysite/settings.py
@@ -74,13 +74,6 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
 if 'RDS_DB_NAME' in os.environ:
     DATABASES = {
         'default': {
