a8886e8c3be3671577fc950274c8f8bdfa132aa4
Duvuru Yathin Reddy <yathinreddy2004@gmail.com>
2026-02-20 21:35:01 -0500

    Allowed All Hosts

--- mysite/settings.py
+++ mysite/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'this-is-a-placeholder-for-loca
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["djangotutorial-dev.us-west-2.elasticbeanstalk.com", "127.0.0.1"]
+ALLOWED_HOSTS = ["djangotutorial-dev.us-west-2.elasticbeanstalk.com", "127.0.0.1" , "*"]
 
 
 # Application definition
